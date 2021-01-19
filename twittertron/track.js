17964d447635b9cfe3ff67dae9caec2f3eb46cb2
Bruce Thomas <burce.thomas@ably.com>
2021-01-19 00:47:40 +0000

    created content filter arrays

--- twittertron/track.js
+++ twittertron/track.js
@@ -1,4 +1,8 @@
-module.exports = [
+// twitter "track" content triggers to watch
+// the incoming message MUST contain one of these phrases
+// OR the profile/message must be in "interesting"
+
+const track = [
     'writingcommunity',
     'writerslift',
     'writerslife',
@@ -15,14 +19,57 @@ module.exports = [
     'drama',
     'scifi',
 
-    'self',
+    'shortstory',
     'published',
-    'short',
-    'story',
-    'flash',
-    'fiction',
+    'flashfiction',
 
     'selfpublished',
     'bookboost',
     'bookplugs',
 ];
+
+const interesting = [
+    '#?(am)?writ(\\w+)', // amwriting, writercommunity
+    '#?edit(\\w+)',
+    '#?film(\\w+)',
+    '#?short(\\w+)',
+    '#?author(\\w+)?',
+    '#?(gho\\w+)?writ(\\w+)', // writting, writer, written
+    '#?screen(\\w+)', // screenwriter, screenplay
+    '#?poe[mtry]+', // poerty, poem, poet
+    '#?book(\\w+)', // book, bookshoop. booklift
+    '#?story(\\w+)', // strory, stroryteller
+    '#?novel(\\w)?', // novella, novel, novelist
+    '#?word(\\w+)',
+    '#?publish\\w+', // publish, published, publisher
+];
+
+// if the profile description OR message contains these
+// the message is discarded immediately
+//
+const ignore = [
+    'blm',
+    'black',
+    'white',
+    'racis(\\w+)',
+    'slav(\\w+)+',
+    'colo(\\w+)',
+    'christ(\\w+)',
+    '(\\w+)?ethic(\\w+)?',
+    'sjw',
+    'equit(\\w+)',
+    'allah',
+    'mohamm(\\w+)',
+    'jesus',
+    'trump',
+    'fascis(\\w+)',
+    'nazi',
+    'suprema(\\w+)',
+    'justice',
+    'misogyn(\\w+)',
+    'patria(\\w+)',
+    'diversi(\\w+)',
+    'extinct(\\w+)',
+];
+
+module.exports = { track, interesting, ignore };
